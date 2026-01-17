a09af9e4036866ac9107f020d8de391402f02695
pinkugoldutibu <sharolarkansas@gmail.com>
2026-01-17 07:57:34 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import turtle
+
+t = turtle.Turtle()
+turtle.bgcolor("lightblue")   # background color
+
+t.color("purple")             # circle color
+t.circle(100)                 # draw circle
+
+turtle.done()
